dc4ca54ffd162b788c4c5e67ac7464079ea262f7
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-09-27 13:30:31 -0700

    n

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import "./app.css";
+import "./App.css";
 
 function App() {
   return (

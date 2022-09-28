56681cdcb22162c8bc3da209d2fe801e62ca21eb
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-09-27 19:10:41 -0700

    Updated the companies with real companys

--- client/src/dummyData.ts
+++ client/src/dummyData.ts
@@ -9,7 +9,7 @@ export const jobs = [
       "Post-ironic selvage chambray sartorial freegan meditation. Chambray chartreuse kombucha meditation, man bun four dollar toast street art cloud bread live-edge heirloom.",
       "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian.",
     ],
-    company: "TOMMY",
+    company: "Trade-x",
   },
   {
     id: "recAGdJdffiU4CeaV0HL",
@@ -21,7 +21,7 @@ export const jobs = [
       "Post-ironic selvage chambray sartorial freegan meditation. Chambray chartreuse kombucha meditation, man bun four dollar toast street art cloud bread live-edge heirloom.",
       "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian.",
     ],
-    company: "TOMMY",
+    company: "Forged",
   },
   {
     id: "recAGJfdfiU4CeaV0HL",
@@ -33,19 +33,7 @@ export const jobs = [
       "Post-ironic selvage chambray sartorial freegan meditation. Chambray chartreuse kombucha meditation, man bun four dollar toast street art cloud bread live-edge heirloom.",
       "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian.",
     ],
-    company: "TOMMY",
-  },
-  {
-    id: "recAGddfJfiU4CeaV0HL",
-    order: 3,
-    title: "Full Stack Web Developer",
-    dates: "December 2015 - Present",
-    duties: [
-      "Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual poke leggings offal cold-pressed brunch neutra. Hammock photo booth live-edge disrupt.",
-      "Post-ironic selvage chambray sartorial freegan meditation. Chambray chartreuse kombucha meditation, man bun four dollar toast street art cloud bread live-edge heirloom.",
-      "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian.",
-    ],
-    company: "TOMMY",
+    company: "Upwork",
   },
   {
     id: "recIL6mJNfWObonls",
@@ -58,7 +46,7 @@ export const jobs = [
       "Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1",
       "Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1",
     ],
-    company: "BIGDROP",
+    company: "Klever",
   },
   {
     id: "rec61x18GVY99hQq5",
@@ -70,30 +58,6 @@ export const jobs = [
       "YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid.",
       "Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism.",
     ],
-    company: "CUKER",
-  },
-  {
-    id: "rec6d1x18GVY99hQq5",
-    order: 1,
-    title: "Engineering Intern",
-    dates: "May 2014 - September 2015",
-    duties: [
-      "I'm baby woke mumblecore stumptown enamel pin. Snackwave prism pork belly, blog vape four loko sriracha messenger bag jean shorts DIY bushwick VHS. Banjo post-ironic hella af, palo santo craft beer gluten-free.",
-      "YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid.",
-      "Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism.",
-    ],
-    company: "CUKER",
-  },
-  {
-    id: "rec61x18GfVY99hQq5",
-    order: 1,
-    title: "Engineering Intern",
-    dates: "May 2014 - September 2015",
-    duties: [
-      "I'm baby woke mumblecore stumptown enamel pin. Snackwave prism pork belly, blog vape four loko sriracha messenger bag jean shorts DIY bushwick VHS. Banjo post-ironic hella af, palo santo craft beer gluten-free.",
-      "YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid.",
-      "Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism.",
-    ],
-    company: "CUKER",
+    company: "HNGi",
   },
 ];

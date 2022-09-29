6760c743d77d93d139cc8ab5046196c5b0b29abe
Nnaji Chimuanya <nnajichimuanyua50@gmail.com>
2022-09-29 04:50:42 -0700

    Fixed the responsiveness

--- client/src/dummyData.ts
+++ client/src/dummyData.ts
@@ -28,11 +28,10 @@ export const jobs: IJob[] = [
     title: "Frontend Engineer",
     dates: "July 2022 - August 2022",
     duties: [
-      "Tote bag sartorial mlkshk air plant vinyl banjo lumbersexual poke leggings offal cold-pressed brunch neutra. Hammock photo booth live-edge disrupt.",
-      "Post-ironic selvage chambray sartorial freegan meditation. Chambray chartreuse kombucha meditation, man bun four dollar toast street art cloud bread live-edge heirloom.",
-      "Butcher drinking vinegar franzen authentic messenger bag copper mug food truck taxidermy. Mumblecore lomo echo park readymade iPhone migas single-origin coffee franzen cloud bread tilde vegan flexitarian.",
+      "Lead the frontend team while debugging and reviewing code. Ensured the team delivered and finished up their tasks making sure that deadline was met",
+      "Implemented the UI for the Dashboard using material UI. Dynamically displayed user's skill progression using a progress bar",
     ],
-    tools: ["TypeScript", "ReactJS", "MaterialUI", "Git"],
+    tools: ["TypeScript", "ReactJS", "MaterialUI", "Git", "team work"],
     company: "Forged",
   },
   {
@@ -42,7 +41,8 @@ export const jobs: IJob[] = [
     dates: "August 2022",
     duties: [
       "Single-handedly implemented the UI of the Dashboard and the other pages while ensuring smooth user flow, ensured safety protocols was implemented and minimal server downtime accrued",
-      "Effective time management to met customer's need and satisfaction",
+      "Created the endpoints for user signup and login using JWT for authentication and authorization, using mongodb database for storing data. Ensured that user data is secured by hashing the password using mongoose pre-save hook. Ensured that certain routes are protected by implemented an isAuthenticated middleware which checks for and verifies the authenticity of the JWT token",
+      "Effective time management to meet customer's need and satisfaction",
     ],
     tools: [
       "TypeScript",
@@ -59,12 +59,9 @@ export const jobs: IJob[] = [
     id: "02",
     order: 2,
     title: "Back-end Engineer",
-    dates: "May 2015 - December 2015",
+    dates: "June 2022 - July 2022",
     duties: [
-      "Hashtag drinking vinegar scenester mumblecore snackwave four dollar toast, lumbersexual XOXO. Cardigan church-key pabst, biodiesel vexillologist viral squid.",
-      "Franzen af pitchfork, mumblecore try-hard kogi XOXO roof party la croix cardigan neutra retro tattooed copper mug. Meditation lomo biodiesel scenester",
-      "Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1",
-      "Fam VHS enamel pin try-hard echo park raw denim unicorn fanny pack vape authentic. Helvetica fixie church-key, small batch jianbing messenger bag scenester +1",
+      "Collaborated with a team of developers to participate in the Klever chain Hackathon developing a decentralized game. As the Backend developer, I designed and implemented the API with endpoints for users to easily interact with the blockchain,create wallet,placing a bet, makings transfers, converting experience points to token, checking wallet Balance e.t.c",
     ],
     tools: [
       "TypeScript",
@@ -75,7 +72,7 @@ export const jobs: IJob[] = [
       "ExpressJS",
       "Git",
     ],
-    company: "Klever",
+    company: "Kodiny Devs",
   },
   {
     id: "01",
